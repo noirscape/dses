c0cac480ea8fa4f8f14a7eadd14ac76d4c5508bc
Ev1l0rd <valentijnvermeij@gmail.com>
2018-06-08 00:26:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## Dead Simple EULA Set
+
+DSES is a simple tool to set the EULAs for the 3DS to accepted.
+
+## What
+
+In order to play 3DS games online/make use of streetpass, the 3DS makes the user accept an EULA. If this is for in-region titles, this is can be handled by the 3DS itself. If you wish to make use of out-of-region titles however, you will need to use homebrew to set the EULA to accepted.
+
+## Why
+
+A number of EULA setting tools exist already (notably, [SciresM/EULASetter](https://github.com/SciresM/EULASetter), which is the base for this tool).
+
+However, all of these tools I've encountered so far sometimes offer a lot of other features and more importantly, put hex values in front of the user.
+
+This tool does none of that. It does _one_ thing. Set your EULA to accepted if it isn't and to unaccepted if it is. No complications, nothing. Just press A and it changes the EULA.
+
+Note that when unaccepting the EULA, you will need to reaccept it the next time you use a 3DS game in your own region.
+
+## Usage
+
+Download the CIA/3DSX from the releases page and open it with FBI. Press A to change your EULA. After that you can remove the application.
---
+++ source/main.c
@@ -0,0 +1,87 @@
+// DSES - Dead Simple EULA Set
+// Copyright (C) 2018 Valentijn "ev1l0rd" V.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#include <stdio.h>
+#include <stdlib.h>
+#include <3ds.h>
+
+int main()
+{
+    // Service initialization
+    aptInit();
+    gfxInitDefault();
+    hidInit();
+    cfguInit();
+    // Variables
+    Result res;
+    bool done = false;
+    uint8_t eulaData[4];
+
+    PrintConsole topScreen;
+    consoleInit(GFX_TOP, &topScreen);
+    consoleSelect(&topScreen);
+
+    printf("Dead Simple EULA Set - v0.1\n\n");
+
+    // read magic
+    res = CFGU_GetConfigInfoBlk2(4, 0xD0000, eulaData);
+    if (R_FAILED(res))
+    {
+        printf("Something weird happened. Couldn't get EULA data.\n\nPress Start to exit.");
+        done = true;
+    }
+
+    if(!done)
+    {
+        if (eulaData[0] == 0xFF || eulaData[1] == 0xFF)
+        {
+            printf("Press A to unset the 3DS EULAs.\n\n");
+        }
+        else
+        {
+            printf("Press A to set the 3DS EULAs.\n\n");
+        }
+    }
+
+    while(aptMainLoop())
+    {
+        hidScanInput();
+        u32 kDown = hidKeysDown();
+        if (kDown & KEY_A || !(done))
+        {
+            if (eulaData[0] == 0xFF || eulaData[1] == 0xFF) // FFFF == EULAs have been set, so unset them.
+                eulaData[0] = eulaData[1] = 0x00;
+            else // Anything else: EULAs have not been set, so set them.
+                eulaData[0] = eulaData[1] = 0xFF;
+            res = CFG_SetConfigInfoBlk8(4, 0xD0000, eulaData);
+            if(R_FAILED(res))
+                printf("Something went wrong...\n\nPress START to exit.\n");
+            else
+                printf("(Un)setting the EULA succeeded.\n\nPress START to exit.\n");
+            done = true;
+        }
+        if (kDown & KEY_START)
+        {
+            printf("Exiting...\n");
+            break;
+        }
+    }
+
+    aptExit();
+    gfxExit();
+    hidExit();
+    cfguExit();
+    return 0;
+}

96393324c6eeeb68d2441a69547e3b9e3913ff2c
Dimitri A. <dimitripilot3@gmail.com>
2024-10-21 17:14:13 +0200

    Implement updating the CFG savegame after setting EULAs
    After writing EULAs to the config info block, call the
    `CFG_UpdateConfigSavegame()` function (which was formerly named
    `CFG_UpdateConfigNANDSavegame()` in older versions of libctru).
    
    This is a code change, mimicking the behaviour of the
    EULASetter app that DSES is based on.
    
    This also tweaks some of the messages printed to the screen.
    
    Fixes an issue in which EULAs weren't guaranteed to persist
    after the system reboots or is powered off.

--- source/main.c
+++ source/main.c
@@ -44,12 +44,15 @@ int main(int argc, char* argv[])
     res = CFGU_GetConfigInfoBlk2(4, 0xD0000, eulaData);
     if (R_FAILED(res))
     {
-        printf("Something weird happened. Couldn't get EULA data.\n\nPress Start to exit.");
+        printf("Something went wrong... Failed to get EULA data.\n\n");
         done = true;
     }
 
+    const char *do_what = eulaData[0] == 0xFF && eulaData[1] == 0xFF ? "unset" : "set";
     if(!done)
-        printf(eulaData[0] == 0xFF && eulaData[1] == 0xFF ? "Press A to unset the 3DS EULAs.\n\n" : "Press A to set the 3DS EULAs.\n\n");
+        printf("Press A to %s the 3DS EULAs.\n\n", do_what);
+
+    printf("Press START to exit.\nPress SELECT to exit & remove this application.\n\n");
 
     while(aptMainLoop())
     {
@@ -61,12 +64,26 @@ int main(int argc, char* argv[])
                 eulaData[0] = eulaData[1] = 0x00;
             else // Anything else: EULAs have not been set, so set them.
                 eulaData[0] = eulaData[1] = 0xFF;
+
+            printf("\e[5A\e[J"); // Erase the last 5 lines printed in the console
+
             res = CFG_SetConfigInfoBlk8(4, 0xD0000, eulaData);
             if(R_FAILED(res))
-                printf("Something went wrong...\n\n");
+                printf("Something went wrong... Failed to %s EULAs.\n\n", do_what);
             else
-                printf(eulaData[0] == 0xFF ? "Setting the EULA succeeded.\n\n" : "Unsetting the EULA succeeded.\n\n");
-            printf("Press START to exit.\nPress Select to exit + remove application.\n");
+            {
+                res = CFG_UpdateConfigSavegame();
+                if (R_SUCCEEDED(res))
+                    printf("Success! EULAs have been %s.\n\n", do_what);
+                else
+                    printf("EULAs have been %s, but a save error occurred.\n\n"
+                           "This should still take effect, but may not\n"
+                           "persist after the system is powered off.\n"
+                           "You can try and adjust the screen brightness\n"
+                           "settings in the HOME Menu right now to force\n"
+                           "the new EULAs to save properly.\n\n", do_what);
+            }
+            printf("Press START to exit.\nPress SELECT to exit & remove this application.\n\n");
             done = true;
         }
         if (kDown & KEY_START)
